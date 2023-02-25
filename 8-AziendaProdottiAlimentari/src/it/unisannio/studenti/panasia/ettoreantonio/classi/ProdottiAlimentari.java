f56aede44b57e5f5f222ed40af5bf02db70837bf
Ettore000 <ettore.panasia@gmail.com>
2023-02-25 15:58:08 +0100

    Update ProdottiAlimentari.java

--- 8-AziendaProdottiAlimentari/src/it/unisannio/studenti/panasia/ettoreantonio/classi/ProdottiAlimentari.java
+++ 8-AziendaProdottiAlimentari/src/it/unisannio/studenti/panasia/ettoreantonio/classi/ProdottiAlimentari.java
@@ -1,5 +1,21 @@
 package it.unisannio.studenti.panasia.ettoreantonio.classi;
 
 public abstract class ProdottiAlimentari {
-
+	public ProdottiAlimentari(String nome, double prezzo) {
+		this.nome=nome;
+		this.prezzo=prezzo;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public double getPrezzo() {
+		return prezzo;
+	}
+	
+	public abstract void descrizioneProdotto();
+	
+	private String nome;
+	private double prezzo;
 }
