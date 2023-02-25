47e63de4ac86e642fbdd4ca765ae52e1c1633f81
Ettore000 <ettore.panasia@gmail.com>
2023-02-25 17:06:31 +0100

    Update StampaProdotti.java

--- 8-AziendaProdottiAlimentari/src/it/unisannio/studenti/panasia/ettoreantonio/scenari/StampaProdotti.java
+++ 8-AziendaProdottiAlimentari/src/it/unisannio/studenti/panasia/ettoreantonio/scenari/StampaProdotti.java
@@ -1,12 +1,20 @@
 package it.unisannio.studenti.panasia.ettoreantonio.scenari;
 
+import it.unisannio.studenti.panasia.ettoreantonio.classi.Pane;
+import it.unisannio.studenti.panasia.ettoreantonio.classi.Pasta;
+import it.unisannio.studenti.panasia.ettoreantonio.classi.Dolce;
 import it.unisannio.studenti.panasia.ettoreantonio.classi.ProdottoAlimentare;
 
 public class StampaProdotti {
 
 	public static void main(String[] args) {
-		ProdottoAlimentare pane=new Pane("Pane integrale", 2.50, "Integrale");
-
+		ProdottoAlimentare pane=new Pane("Rosetta", 2.50, "Integrale");
+		ProdottoAlimentare pasta=new Pasta("Spaghetti", 1.50, "Lunghi");
+		ProdottoAlimentare dolce=new Dolce("Tiramisu'", 3.50, "Caffe'");
+		
+		pane.descrizioneProdotto();
+		pasta.descrizioneProdotto();
+		dolce.descrizioneProdotto();
 	}
 
 }
