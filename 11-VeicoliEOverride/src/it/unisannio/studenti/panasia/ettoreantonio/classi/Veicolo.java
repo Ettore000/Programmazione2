dc3b9bff39fcf91973e15a19d4e040ba2b461950
Ettore000 <ettore.panasia@gmail.com>
2023-02-26 12:23:02 +0100

    Override

---
+++ 11-VeicoliEOverride/src/it/unisannio/studenti/panasia/ettoreantonio/classi/Veicolo.java
@@ -0,0 +1,14 @@
+package it.unisannio.studenti.panasia.ettoreantonio.classi;
+
+public class Veicolo {
+	public Veicolo(int velocita) {
+		super();
+		this.velocita = velocita;
+	}
+	
+	public int getVelocita() {
+		return velocita;
+	}
+	
+	private int velocita;
+}
