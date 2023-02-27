4f54b7b32241a0ad47b2e8d479e16733257859b0
Ettore000 <ettore.panasia@gmail.com>
2023-02-27 23:12:13 +0100

    Edit

---
+++ B18-ExceptionHandling/src/it/unisannio/studenti/panasia/ettoreantonio/classi/NegativeNumberException.java
@@ -0,0 +1,9 @@
+package it.unisannio.studenti.panasia.ettoreantonio.classi;
+
+public class NegativeNumberException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public NegativeNumberException() {
+		super();
+	}
+}
---
+++ B18-ExceptionHandling/src/it/unisannio/studenti/panasia/ettoreantonio/tester/CuboNumero.java
@@ -0,0 +1,29 @@
+package it.unisannio.studenti.panasia.ettoreantonio.tester;
+
+import java.util.Scanner;
+
+import it.unisannio.studenti.panasia.ettoreantonio.classi.NegativeNumberException;
+
+public class CuboNumero {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Inserisci un numero intero: ");
+		try {
+			/*
+			 *  Il metodo nextInt() solleverà automaticamente un'eccezione di tipo InputMismatchException
+			 */
+			int num=sc.nextInt();
+			if(num<0)throw new NegativeNumberException(); //Eccezione personalizzata definita in una Classe
+			
+			int cubo=num*num*num;
+			System.out.println("Il cubo di "+num+" e' "+cubo);
+		} catch (NegativeNumberException e) {
+			System.err.println("Errore: il numero inserito e' negativo");
+		} catch (Exception e) {
+			System.err.println("Errore: devi inserire un numero intero");
+		}
+	}
+
+}
