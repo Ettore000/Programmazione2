4b6b8256cc0f3787dc9c3b01a4f34259017a09b1
Ettore000 <ettore.panasia@gmail.com>
2023-02-28 10:48:33 +0100

    Edit

---
+++ A6-Banca/src/it/unisannio/studenti/panasia/ettoreantonio/tester/BancaTest.java
@@ -0,0 +1,10 @@
+package it.unisannio.studenti.panasia.ettoreantonio.tester;
+
+public class BancaTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
