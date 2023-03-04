f0f42194e0814b604c88059284fe7c3e937163ed
Ettore000 <ettore.panasia@gmail.com>
2023-03-04 17:34:50 +0100

    Edit

--- B24-LinkedMap/src/it/unisannio/studenti/panasia/ettoreantonio/classi/Studente.java
+++ B24-LinkedMap/src/it/unisannio/studenti/panasia/ettoreantonio/classi/Studente.java
@@ -1,5 +1,22 @@
 package it.unisannio.studenti.panasia.ettoreantonio.classi;
 
 public class Studente {
+	public Studente(String nome, String cognome, double media) {
+		this.nome = nome;
+		this.cognome = cognome;
+		this.media = media;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public double getMedia() {
+		return media;
+	}
 
+	private String nome, cognome;
+	private double media;
 }
--- B24-LinkedMap/src/it/unisannio/studenti/panasia/ettoreantonio/tester/StudenteTest.java
+++ B24-LinkedMap/src/it/unisannio/studenti/panasia/ettoreantonio/tester/StudenteTest.java
@@ -1,9 +1,23 @@
 package it.unisannio.studenti.panasia.ettoreantonio.tester;
 
+import java.util.LinkedHashMap;
+
+import it.unisannio.studenti.panasia.ettoreantonio.classi.Studente;
+
 public class StudenteTest {
 
 	public static void main(String[] args) {
+		//Creiamo la LinkedHashMap
+		LinkedHashMap<String, Studente> studenti=new LinkedHashMap<String, Studente>();
 		
+		//Aggiungiamo alcuni studenti alla mappa
+		studenti.put("Mario", new Studente("Mario", "Rossi", 7.5));
+		studenti.put("Paolo", new Studente("Paolo", "Neri", 8.0));
+		studenti.put("Luca", new Studente("Luca", "Verdi", 6.5));
+		
+		//Stampiamo i nomi e le medie degli studenti in ordine di inserimento
+		System.out.println("Nomi e medie degli studenti in ordine di inserimento: ");
+		for(String nome:studenti.keySet())System.out.println(nome+": "+studenti.get(nome).getMedia());
 	}
 
 }
