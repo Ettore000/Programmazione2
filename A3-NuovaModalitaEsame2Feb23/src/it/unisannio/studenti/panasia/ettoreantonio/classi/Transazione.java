aafb45539f8cc2b37afa3f3c90ff5022901fa489
Ettore000 <ettore.panasia@gmail.com>
2023-03-14 22:01:28 +0100

    Edit

--- A3-NuovaModalitaEsame2Feb23/src/it/unisannio/studenti/panasia/ettoreantonio/classi/Transazione.java
+++ A3-NuovaModalitaEsame2Feb23/src/it/unisannio/studenti/panasia/ettoreantonio/classi/Transazione.java
@@ -1,16 +1,21 @@
 package it.unisannio.studenti.panasia.ettoreantonio.classi;
 
 import java.io.PrintStream;
+import java.util.HashMap;
 import java.util.Map;
 
 public abstract class Transazione {
 	public Transazione(int id, String data) {
 		this.id=id;
 		this.data=data;
+		this.transazioniOnline=new HashMap<>();
+		this.transazioniFisiche=new HashMap<>();
 	}
 
 	public int getId(){return id;}
 	public String getData(){return data;}
+	public Map<String, TransazioneOnline> getTransazioniOnline(){return transazioniOnline;}
+	public Map<String, TransazioneFisica> getTransazioniFisiche(){return transazioniFisiche;}
 	public abstract Map<String, Libro> getLibri();
 	public abstract void addLibri(Libro libro);
 	public abstract double costoTotale();
@@ -19,4 +24,6 @@ public abstract class Transazione {
 
 	private int id;
 	private String data;
+	private Map<String, TransazioneOnline> transazioniOnline;
+	private Map<String, TransazioneFisica> transazioniFisiche;
 }
