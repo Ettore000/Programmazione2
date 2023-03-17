dafa0e64352f7043409654421fa5d5a0769c1247
Ettore000 <ettore.panasia@gmail.com>
2023-03-17 17:00:44 +0100

    Update Transazione.java

--- A3-NuovaModalitaEsame2Feb23/src/it/unisannio/studenti/panasia/ettoreantonio/classi/Transazione.java
+++ A3-NuovaModalitaEsame2Feb23/src/it/unisannio/studenti/panasia/ettoreantonio/classi/Transazione.java
@@ -1,29 +1,45 @@
 package it.unisannio.studenti.panasia.ettoreantonio.classi;
 
 import java.io.PrintStream;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-public abstract class Transazione {
-	public Transazione(int id, String data) {
+public class Transazione {
+	public Transazione(int id, Date data) {
 		this.id=id;
 		this.data=data;
-		this.transazioniOnline=new HashMap<>();
-		this.transazioniFisiche=new HashMap<>();
+		this.libri=new HashMap<>();
 	}
 
-	public int getId(){return id;}
-	public String getData(){return data;}
-	public Map<String, TransazioneOnline> getTransazioniOnline(){return transazioniOnline;}
-	public Map<String, TransazioneFisica> getTransazioniFisiche(){return transazioniFisiche;}
-	public abstract Map<String, Libro> getLibri();
-	public abstract void addLibri(Libro libro);
-	public abstract double costoTotale();
-	public abstract void print();
-	public abstract void printFile(PrintStream nomeFile);
+	public int getId(){
+		return id;
+	}
+
+	public Date getData(){
+		return data;
+	}
+
+	public Map<String, Libro> getLibri() {
+		
+	}
+	public abstract void addLibri(Libro libro) {
+		
+	}
+	
+	public abstract double costoTotale() {
+		
+	}
+	
+	public abstract void print() {
+		
+	}
+	
+	public abstract void printFile(PrintStream nomeFile) {
+		
+	}
 
 	private int id;
-	private String data; //TODO TIPO DATE
-	private Map<String, TransazioneOnline> transazioniOnline;//TODO non servono
-	private Map<String, TransazioneFisica> transazioniFisiche;
+	private Date data;
+	private Map<String, Libro> libri;
 }
