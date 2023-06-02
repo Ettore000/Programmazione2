755111c97d5eb9cc71606fc8d84463a8c25c92ca
Ettore000 <ettore.panasia@gmail.com>
2023-06-03 00:47:16 +0200

    Create Main.java

---
+++ B32-Introduzione/src/Main.java
@@ -0,0 +1,35 @@
+/*
+ * Perchè imparare Java:
+ * 
+ * - Top 3 linguaggi più popolari
+ * - Usato a livello aziendale (framework Spring), giochi (Minecraft), web app e android
+ * 
+ * Come funziona:
+ * 
+ * - computer usano codice macchina (basso livello) binario 00000001 00000010 00000011 00000100
+ * - noi scriviamo source code (alto livello) in file .java
+ * - java viene poi compilato ovvero tradotto in codice macchina
+ * - quando però compiliamo il codice è specifico per il sistema operativo (Mac, Windows ecc..)
+ * - java mette a disposizione uno step intermedio
+ * 		source code --> compiler --> byte code --> jvm --> machine code
+ * 
+ * JVM: java virtual machine è contenuto nel JDK
+ * JDK: java development kit che contiene JRE
+ * JRE: java runtime environment contiene librerie e toolkits
+ * 
+ * JDK --> JRE --> JVM
+ * 
+ * Ci serve un IDE
+ * IDE: integrate development environment (ambiente di sviluppo integrato)
+ * - eclipse, intellij e vsc
+ * 
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
