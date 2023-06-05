1736063f8c0bf36e33b7eda1c41120f0ad59a1a0
Ettore000 <ettore.panasia@gmail.com>
2023-06-05 15:57:22 +0200

    Update Main.java

--- B32-Introduzione/src/lezioneb32_modificatoriDiAccesso/Main.java
+++ B32-Introduzione/src/lezioneb32_modificatoriDiAccesso/Main.java
@@ -1,3 +1,10 @@
+/* Lezione 32
+ * - Modificatori di accesso
+ * 
+ * - modificatori di accesso servono per aggiungere un livello di sicurezza aggiuntivo
+ * - creare 2 package con 2 classi
+ */
+
 package lezioneb32_modificatoriDiAccesso;
 
 public class Main {
