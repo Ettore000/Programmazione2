9e02324cc628a09b83548e3bd67ab3296d6ef158
Ettore000 <ettore.panasia@gmail.com>
2023-06-11 00:20:48 +0200

    Update Museo.java

--- B41-EsercizioMuseo/src/classi/Museo.java
+++ B41-EsercizioMuseo/src/classi/Museo.java
@@ -1,6 +1,13 @@
 package classi;
 
+import java.util.ArrayList;
+
 public class Museo {
+	public Museo() {
+		this.libri=new ArrayList<Libro>();
+		this.stampe=new ArrayList<Stampa>();
+		this.opereMultimediali=new ArrayList<OperaMultimediale>();
+	}
 	/*
 	 * Un museo ha organizzato il proprio archivio in tre File, come segue:
 			Libri				(Autore	//	Titolo	//	anno		//	collocazione)
@@ -15,4 +22,25 @@ public class Museo {
 			anno
 			una qualsiasi combinazione di tali fattori
 	 */
+	
+	public ArrayList<Libro> getLibri() {
+		return libri;
+	}
+	public ArrayList<Stampa> getStampe() {
+		return stampe;
+	}
+	public ArrayList<OperaMultimediale> getOpereMultimediali() {
+		return opereMultimediali;
+	}
+	
+	public static ArrayList<Libro> cercaLibri(String criterio, String valore) {
+		boolean trovato=false;
+		Libro l;
+		int i=0;
+		
+		//TODO
+	}
+	private ArrayList<Libro> libri;
+	private ArrayList<Stampa> stampe;
+	private ArrayList<OperaMultimediale> opereMultimediali;
 }
