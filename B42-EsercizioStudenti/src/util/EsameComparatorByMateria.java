95778a0fbdb02792a3cc58c2aaca5746f5baf4ed
Ettore000 <ettore.panasia@gmail.com>
2023-06-23 16:37:39 +0200

    Studio

--- B42-EsercizioStudenti/src/util/EsameComparatorByMateria.java
+++ B42-EsercizioStudenti/src/util/EsameComparatorByMateria.java
@@ -7,12 +7,13 @@ import classi.Esame;
 /**
  * Modella metodi speciali di confronto degli attributi di un esame
  */
-public class EsameComparator implements Comparator<Esame> {
+public class EsameComparatorByMateria implements Comparator<Esame> {
 
 	@Override
 	public int compare(Esame e1, Esame e2) {
-		// TODO Auto-generated method stub
-		return 0;
+		if(e1.getMateria()<e2.hashCode())return -1;
+		if(e1.hashCode()==e2.hashCode())return 0;
+		return 1;
 	}
 
 }
