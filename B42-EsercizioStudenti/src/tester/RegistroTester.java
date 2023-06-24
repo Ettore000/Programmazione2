16d12a2468cff0c7ccd7ae49056278ed94371b0e
Ettore000 <ettore.panasia@gmail.com>
2023-06-24 15:29:09 +0200

    Studio

--- B42-EsercizioStudenti/src/tester/RegistroTester.java
+++ B42-EsercizioStudenti/src/tester/RegistroTester.java
@@ -1,9 +1,21 @@
 package tester;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import classi.Registro;
+import util.Costante;
+
+/**
+ * Test driver della classe Registro
+ */
 public class RegistroTester {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
+		Registro registro=new Registro(new Scanner(new File(Costante.FILE_STUDENTI)), new Scanner(new File(Costante.FILE_ESAMI)));
 		
+		registro.
 	}
 
 }
