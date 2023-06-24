900f38dd766c722430f60ed7b70f6894605a405d
Ettore000 <ettore.panasia@gmail.com>
2023-06-24 11:27:03 +0200

    Studio

--- B42-EsercizioStudenti/src/tester/StudenteTester.java
+++ B42-EsercizioStudenti/src/tester/StudenteTester.java
@@ -1,33 +1,51 @@
 package tester;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 import classi.Esame;
 import classi.Studente;
+import util.Costante;
 
 /**
  * Test driver della classe studente
  */
 public class StudenteTester {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		//input da tastiera
-		Studente studente=Studente.read(); //inserimento studente da tastiera
-		studente.addEsame(Esame.read()); //inserimento esame da tastiera
+		Studente s1=Studente.read(); //inserimento studente da tastiera
+		s1.addEsame(Esame.read()); //inserimento esame da tastiera
 		
-		System.out.println("Nome: "+studente.getNome());
-		System.out.println("Cognome: "+studente.getCognome());
-		System.out.println("Matricola: "+studente.getMatricola());
-		System.out.println("Numero esami: "+studente.getNumeroEsami());
-		System.out.println("Lista esami: "+studente.getEsami());
+		System.out.println("Nome="+s1.getNome());
+		System.out.println("Cognome="+s1.getCognome());
+		System.out.println("Matricola="+s1.getMatricola());
+		System.out.println("Numero esami="+s1.getNumeroEsami());
+		System.out.println("Lista esami="+s1.getEsami());
 		
-		Esame esame=studente.getEsami().get(0); //individuazione esame
-		studente.removeEsame(esame); //rimozione esame
-		System.out.println("Numero esami dopo la rimozione: "+studente.getNumeroEsami());
-		System.out.println("Lista esami dopo la rimozione: "+studente.getEsami());
+		Esame e1=s1.getEsami().get(0); //individuazione esame
+		s1.removeEsame(e1); //rimozione esame
+		System.out.println("Numero esami dopo la rimozione="+s1.getNumeroEsami());
+		System.out.println("Lista esami dopo la rimozione="+s1.getEsami());
 		
-		studente.print(); //visualizzazione generica su schermo
+		s1.print(); //visualizzazione generica su schermo
 		
-		System.out.println(studente.toString()); //visualizzazione su schermo dell'oggetto
+		System.out.println(s1.toString()); //visualizzazione su schermo dell'oggetto
 		
-		System.out.println(studente.hashCode()); //visualizzazione su schermo dell'identificatre univoco dell'oggetto
+		System.out.println("HashCode s1="+s1.hashCode()); //visualizzazione su schermo dell'identificatre univoco dell'oggetto
+		
+		s1.print(new PrintStream(Costante.FILE_STUDENTI)); //scrittura su file
+		
+		//input da file
+		Studente s2=Studente.read(new Scanner(new File(Costante.FILE_STUDENTI))); //lettura studente da file
+		System.out.println("-------------------------------");
+		System.out.println(s2.toString());
+		System.out.println("HashCode s2="+s1.hashCode());
+		
+		//confronto
+		System.out.println(s1.equals(s2)); //se sono uguali restituisce true
+		System.out.println(s1.compareTo(s2)); //restituisce -1, 0 e 1 a seconda dell'ordinamento
 	}
 
 }
--- B42-EsercizioStudenti/src/util/Costante.java
+++ B42-EsercizioStudenti/src/util/Costante.java
@@ -5,4 +5,6 @@ public interface Costante {
 	public static final int VOTO_MINIMO = 18;
 	public static final int VOTO_MASSIMO = 30;
 	public static final int NUMERO_PRIMO = 31;
+	public static final String FILE_STUDENTI = "studenti.txt";
+	public static final String FILE_ESAMI = "esami.txt";
 }
